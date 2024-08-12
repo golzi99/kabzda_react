2e247b2433f5c9611a7a5d4dc2931f6d7a519b71
Pavel <golzi99@gmail.com>
2024-08-12 16:14:52 +0300

    12.08.2024 microTasks

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,29 @@ import './App.css';
 import {Accordion} from './components/accordion/Accordion';
 import {Rating} from "./components/rating/Rating";
 
+// type MoneyType = {
+//     banknots: string,
+//     value: number,
+//     number: string
+// }
+//
+// type ButtonMoneyType = "all" | "rubles" | "dollars"
+
 function App() {
-  return (
-    <div className="App">
-        <PageTitle title={"This is APP component"}/>
-        <Rating value={3}/>
-        <Accordion title={"Menu"} />
-        <Accordion title={"Users"} collapsed/>
-        <Rating value={0}/>
-        <Rating value={1}/>
-        <Rating value={2}/>
-        <Rating value={3}/>
-        <Rating value={4}/>
-        <Rating value={5}/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <PageTitle title={"This is APP component"}/>
+            <Rating value={3}/>
+            <Accordion title={"Menu"} />
+            <Accordion title={"Users"} collapsed/>
+            <Rating value={0}/>
+            <Rating value={1}/>
+            <Rating value={2}/>
+            <Rating value={3}/>
+            <Rating value={4}/>
+            <Rating value={5}/>
+        </div>
+    );
 }
 
 type PageTitlePropsType = {
@@ -31,3 +39,72 @@ const PageTitle = (props: PageTitlePropsType) => {
 }
 
 export default App;
+
+
+//    const [money, setMoney] = useState([
+//         {banknots: 'Dollars', value: 100, number: ' a1234567890'},
+//         {banknots: 'Dollars', value: 50, number: ' z1234567890'},
+//         {banknots: 'RUBLS', value: 100, number: ' w1234567890'},
+//         {banknots: 'Dollars', value: 100, number: ' e1234567890'},
+//         {banknots: 'Dollars', value: 50, number: ' c1234567890'},
+//         {banknots: 'RUBLS', value: 100, number: ' r1234567890'},
+//         {banknots: 'Dollars', value: 50, number: ' x1234567890'},
+//         {banknots: 'RUBLS', value: 50, number: ' v1234567890'},
+//     ] as Array<MoneyType>)
+//
+//     const[filter, setFilter] = useState("all" as ButtonMoneyType)
+//
+//     let filtredMoney: Array<MoneyType> = money
+//
+//     if (filter === "rubles") {
+//         filtredMoney = money.filter(typeMoney => typeMoney.banknots === "RUBLS")
+//     } else if (filter === "dollars") {
+//         filtredMoney = money.filter(typeMoney => typeMoney.banknots === "Dollars")
+//     }
+//
+//     const onClickFilterMoneyHandler = (nameButton: ButtonMoneyType) => {
+//         setFilter(nameButton)
+//     }
+//
+//     return (
+//         <div className="App">
+//             <ul>
+//                 {filtredMoney.map((obj, index) => {
+//                     return (
+//                         <li key={index}>
+//                             <span> {obj.banknots}</span>
+//                             <span> {obj.value}</span>
+//                             <span> {obj.number}</span>
+//                         </li>
+//                     )
+//                 })}
+//             </ul>
+//             <button onClick={() => onClickFilterMoneyHandler("all")}>all</button>
+//             <button onClick={() => onClickFilterMoneyHandler("rubles")}>rubles</button>
+//             <button onClick={() => onClickFilterMoneyHandler("dollars")}>dollars</button>
+
+
+// const [money, setMoney] = useState([
+//     {banknots: 'Dollars', value: 100, number: ' a1234567890'},
+//     {banknots: 'Dollars', value: 50, number: ' z1234567890'},
+//     {banknots: 'RUBLS', value: 100, number: ' w1234567890'},
+//     {banknots: 'Dollars', value: 100, number: ' e1234567890'},
+//     {banknots: 'Dollars', value: 50, number: ' c1234567890'},
+//     {banknots: 'RUBLS', value: 100, number: ' r1234567890'},
+//     {banknots: 'Dollars', value: 50, number: ' x1234567890'},
+//     {banknots: 'RUBLS', value: 50, number: ' v1234567890'},
+// ] as Array<MoneyType>)
+//
+// const[filter, setFilter] = useState("all" as ButtonMoneyType)
+//
+// let filtredMoney: Array<MoneyType> = money
+//
+// if (filter === "rubles") {
+//     filtredMoney = money.filter(typeMoney => typeMoney.banknots === "RUBLS")
+// } else if (filter === "dollars") {
+//     filtredMoney = money.filter(typeMoney => typeMoney.banknots === "Dollars")
+// }
+//
+// const onClickFilterMoneyHandler = (nameButton: ButtonMoneyType) => {
+//     setFilter(nameButton)
+//}
